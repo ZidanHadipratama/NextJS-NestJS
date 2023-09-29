5c1a9ae649ee1f562d712831b6ca94daac0980ff
Zidan Hadipratama <zzzdn.hadi@gmail.com>
2023-09-29 15:49:44 +0700

    Add To Do Resource

--- next-nest-js/src/app.module.ts
+++ next-nest-js/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [ConfigModule, ConfigModule.forRoot({ envFilePath: ['.env'] })],
+  imports: [ConfigModule, ConfigModule.forRoot({ envFilePath: ['.env'] }), TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
